8a8bcf60e4d7742f143a0868d5cb8530b6427f08
Herdian Sc <herdiansc@gmail.com>
2015-12-13 23:38:45 +0700

    FEATURE: Add Pencarian Populer

--- config/development.js
+++ config/development.js
@@ -8,7 +8,7 @@ module.exports = {
 	    url: 'http://localhost:3000',
 	    slogan: 'Kunjungi Tempat Wisata Liburan Murah',
 	    description: 'Cari temukan objek wisata eksotis seluruh Indonesia yang wajib dikunjungi untuk destinasi liburan & tour anda hanya di XplOOor.com',
-	    static_host: '/images'
+	    static_host: 'http://localhost:3000'
 	},
 	folders: {
 		root_folder: '/home/herdiansc/www/web_scraping/site/tripkoo',
